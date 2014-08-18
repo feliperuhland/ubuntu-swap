b3032f32ab367ffc37eb224c8ed992c3aab44921
Felipe Ruhland <felipe.ruhland@gmail.com>
2014-08-18 20:58:28 -0300

    Created script for swap

---
+++ create_swap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab
